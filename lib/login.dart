31cdd44622019d585a947624e44ffb2dd7738e19
Xiang Weng <xiang.weng3@gmail.com>
2021-01-16 17:02:56 -0500

    Wave Effect Login

--- lib/login.dart
+++ lib/login.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wood_walk/textfield_widget.dart';
+import 'package:wood_walk/wave_widget.dart';
 
 import 'button_widget.dart';
 
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return SafeArea(
       child: Scaffold(
         body: Stack(
           children: <Widget>[
             //Logo here
+            Container(
+              height: size.height - 200,
+              color: Colors.blue,
+            ),
+            WaveWidget(
+              size: size,
+              yOffset: size.height / 3.0,
+              color: Colors.white,
+            ),
             Padding(
               padding: const EdgeInsets.all(30.0),
               child: Column(
