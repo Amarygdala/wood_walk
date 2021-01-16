bb313c1aec8f070da8e8ef5e85451c146934e705
Xiang Weng <xiang.weng3@gmail.com>
2021-01-16 14:36:02 -0500

    Login page

---
+++ lib/login.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:wood_walk/textfield_widget.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            TextFieldWidget(
+              hintText: 'Email',
+              obscureText: false,
+              prefixIconData: Icons.mail_outline,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            TextFieldWidget(
+              hintText: 'Password',
+              obscureText: false,
+              prefixIconData: Icons.lock_outline,
+            ),
+            SizedBox(
+              height: 20.0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
