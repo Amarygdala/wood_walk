39bf6d05b9bbda4807890674c4924c8f53f41029
Xiang Weng <xiang.weng3@gmail.com>
2021-01-16 15:46:28 -0500

    safe area login

--- lib/login.dart
+++ lib/login.dart
@@ -7,60 +7,62 @@ import 'button_widget.dart';
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          //Logo here
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextFieldWidget(
-                  hintText: 'Email',
-                  obscureText: false,
-                  prefixIconData: Icons.mail_outline,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextFieldWidget(
-                      hintText: 'Password',
-                      obscureText: false,
-                      prefixIconData: Icons.lock_outline,
-                    ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    Text(
-                      'Forgot password?',
-                      style: TextStyle(
-                        color: Colors.blue,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            //Logo here
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  TextFieldWidget(
+                    hintText: 'Email',
+                    obscureText: false,
+                    prefixIconData: Icons.mail_outline,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      TextFieldWidget(
+                        hintText: 'Password',
+                        obscureText: false,
+                        prefixIconData: Icons.lock_outline,
                       ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ButtonWidget(
-                  title: 'Login',
-                  hasBorder: false,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                ButtonWidget(
-                  title: 'Sign Up',
-                  hasBorder: true,
-                ),
-              ],
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'Forgot password?',
+                        style: TextStyle(
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ButtonWidget(
+                    title: 'Login',
+                    hasBorder: false,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ButtonWidget(
+                    title: 'Sign Up',
+                    hasBorder: true,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
