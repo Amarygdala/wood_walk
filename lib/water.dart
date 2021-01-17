020280e2ff4e22e997d5ba8ccd551ede2f542acf
Xiang Weng <xiang.weng3@gmail.com>
2021-01-16 23:45:29 -0500

    water nice

--- lib/water.dart
+++ lib/water.dart
@@ -1,14 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:wood_walk/wave_widget.dart';
+import 'package:intl/intl.dart';
 
 int index = 0;
 
-class Water extends StatelessWidget {
+class Water extends StatefulWidget {
+  @override
+  _WaterState createState() => _WaterState();
+}
+
+class _WaterState extends State<Water> {
+  DateTime now;
+  Timer _everySecond;
+  String formattedDate;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // sets first value
+    now = DateTime.now();
+
+    // defines a timer
+    _everySecond = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      setState(() {
+        now = DateTime.now();
+        formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
+    formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
+    var _dailyDrop;
+    _dailyDrop = 666;
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
@@ -51,7 +81,7 @@ class Water extends StatelessWidget {
           ),
         body: Stack(
             children: <Widget>[
-
+              //date and time
               Container(
                 height: size.height - 200,
                 color: Colors.blue,
@@ -64,11 +94,55 @@ class Water extends StatelessWidget {
               Container(
                 decoration: BoxDecoration(
                     image: DecorationImage(
-                      image: AssetImage('assets/water_background2.png'),
+                      image: AssetImage('assets/water_page4.png'),
                       fit: BoxFit.cover,
                     )
                 ),
               ),
+              Column(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Icon(Icons.access_alarm),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Center(child: Text(formattedDate, textAlign: TextAlign.center,style: new TextStyle(fontWeight: FontWeight.bold,fontSize: 25.0),)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 5,
+                    child: Center(
+                      child: Text(
+                      'Daily:\n$_dailyDrop Drops',
+                        style: TextStyle(
+                          fontFamily: 'Abel',
+                          fontSize: 42,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Lifetime:\n$_dailyDrop Drops',
+                      style: TextStyle(
+                        fontFamily: 'Abel',
+                        fontSize: 42,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                ),
+              ]
+      ),
             ],
         ),
       )
