b1252b9817f43f399e74efd8904d46976927cd49
Xiang Weng <xiang.weng3@gmail.com>
2021-01-17 01:14:58 -0500

    stepss

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Stream<StepCount> _stepCountStream;
+  String _steps = '?';
 
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() {
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
 
   int index = 0;
 
@@ -81,6 +109,11 @@ class _MyAppState extends State<MyApp> {
                           'Steps taken:',
                           style: TextStyle(fontSize: 20),
                         ),
+                         Text(
+                          _steps,
+                          style: TextStyle(fontSize: 30),
+
+                      ),
                     ],
                   ),
 //              Divider(
